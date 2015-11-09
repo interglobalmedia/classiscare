40ba181dc961c8f52625a2d7aec9f727599ba7da
interglobalmedia <interglobalmedia@gmail.com>
2015-11-09 13:52:21 -0500

    Committing changes to drawer.js. This is where I had to add code to make the drawer work. I've added comments stating why it didn't happen at first, and what I had to do to get there.

--- js/drawer.js
+++ js/drawer.js
@@ -7,34 +7,11 @@ $(document).ready(function() {
 // API Methods
 
 // Option
-$(".drawer").drawer({
+// I had trouble following instructions here. But what I finally figured out was if I wanted to add an option, 
+// all I had to do was to call the option here with the .drawer() method. I didn't need an api method because
+// I am not linking to social media like Blevista does on their site. All the other unncessary code I put in
+// here previously was blocking the X from working. If I have time, I will play around with the other options
+// to see what they do. Maybe we'll do it as a group!
+$('.drawer').drawer({
   apiToggleClass: "drawer-toggle"
 });
-
-// Open
-$('.drawer').on("click", function() {
-  $('.drawer').drawer('open');
-});
-
-// close
-$('.drawer').on(function() {
-  $('.drawer').drawer('close');
-});
-
-// toggle
-$('.drawer-toggle').on(function() {
-  $('.drawer').drawer('toggle');
-});
-
-// destroy
-$('.drawer').on(function() {
-  $('.drawer').drawer('destroy');
-});
-
-// Events
-
-// Opened
-$('#drawer').on('drawer.opened');
-
-// Closed
-$('#drawer').on('drawer.closed');
